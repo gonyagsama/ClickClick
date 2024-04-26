5e97792833e8e6d20d42d49754900cbece9f666e
Leejaerin <aaa1967200521@gmail.com>
2024-04-26 14:06:37 +0900

    외계인가나디애니

---
+++ Assets/2_Scripts/Play.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Play : MonoBehaviour
+{
+    public void SceneChange()
+    {
+        SceneManager.LoadScene("PlayScenes");
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
